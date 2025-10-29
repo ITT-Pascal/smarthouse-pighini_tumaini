0f6540044fda70205f5ae8a91b550fe5ab1fc741
pietro.pighini <pietro.pighini.stud@ispascalcomandini.it>
2025-10-29 10:58:52 +0100

    modellazione classe lamp

--- SmartHouse/src/SmartHouse.Domain/Lamp.cs
+++ SmartHouse/src/SmartHouse.Domain/Lamp.cs
@@ -1,7 +1,18 @@
-﻿namespace SmartHouse.Domain
+﻿using System.ComponentModel;
+
+namespace SmartHouse.Domain
 {
     public class Lamp
     {
         public bool IsOn { get; private set; }
+        public int Brightness { get; private set; } 
+        public string Color { get; private set; }
+        public Lamp()
+        {
+            IsOn = false;
+            Brightness = 0;
+            Color = "White";
+        }
+
     }
 }
