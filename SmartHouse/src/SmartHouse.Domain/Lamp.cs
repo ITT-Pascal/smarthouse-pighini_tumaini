d7e585b4dafa702d07ffd418220e57c417749346
GabrieleTumaini <gabriele.tumaini.stud@ispascalcomandini.it>
2025-10-31 08:10:47 +0100

    Class Lamp modified and tests of Lamp added

--- SmartHouse/src/SmartHouse.Domain/Lamp.cs
+++ SmartHouse/src/SmartHouse.Domain/Lamp.cs
@@ -1,18 +1,32 @@
-﻿using System.ComponentModel;
-
-namespace SmartHouse.Domain
+﻿namespace BlaisePascal.SmartHouse.Domain
 {
     public class Lamp
     {
         public bool IsOn { get; private set; }
-        public int Brightness { get; private set; } 
-        public string Color { get; private set; }
+        public int BrightnessLevel { get; private set; }
+        public int MaxBrightnessLevel { get; private set; } = 100;
+
         public Lamp()
         {
             IsOn = false;
-            Brightness = 0;
-            Color = "White";
         }
 
+        public void TurnOff()
+        {
+            IsOn = false;
+        }
+        public void TurnOn()
+        {
+            IsOn = true;
+        }
+
+        public void SetBrightness(int levelOfBrightness)
+        {
+            if (levelOfBrightness < 0 || levelOfBrightness > MaxBrightnessLevel)
+            {
+                throw new ArgumentOutOfRangeException("Brightness level must be between 0 and MaxBrightnessLevel.");
+            }
+            BrightnessLevel = levelOfBrightness;
+        }
     }
 }
