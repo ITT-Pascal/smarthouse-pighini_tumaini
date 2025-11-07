206ccd6396c063f31a76587cd4459356d876b168
GabrieleTumaini <gabriele.tumaini.stud@ispascalcomandini.it>
2025-11-07 08:49:35 +0100

    Added tests of TwoLampDevice

--- SmartHouse/test/BlaisePascal.SmarthouseTest/TwoLampDeviceTest.cs
+++ SmartHouse/test/BlaisePascal.SmarthouseTest/TwoLampDeviceTest.cs
@@ -9,33 +9,55 @@ namespace BlaisePascal.SmartHouse.Domain.UnitTest
     public class TwoLampDeviceTest
     {
         [Fact]
-        public void TwoLampDevice_TurnOnOneLamp_SetsIsOnToTrue()
+        public void TwoLampDevice_TurnOnOneLamp_1_SetsIsOnToTrueTheOtherRemainsFalse()
         {
             TwoLampDevice device = new TwoLampDevice();
             device.TurnOnOneLamp(device.lamp1);
             Assert.True(device.lamp1.IsOn);
             Assert.False(device.lamp2.IsOn);
         }
+
         [Fact]
-        public void TwoLampDevice_TurnOffOneLamp_SetsIsOnToFalse()
+        public void TwoLampDevice_TurnOnOneLamp_2_SetsIsOnToTrueTheOtherRemainsFalse()
         {
             TwoLampDevice device = new TwoLampDevice();
-            device.TurnBothLampsOn();
-            device.TurnOffOneLamp(device.lamp1);
-            Assert.False(device.lamp1.IsOn);
+            device.TurnOnOneLamp(device.lamp2);
             Assert.True(device.lamp2.IsOn);
+            Assert.False(device.lamp1.IsOn);
         }
 
         [Fact]
-        public void TwoLampDevice_TurnOnBothLamps_SetsIsOnToTrue()
+        public void TwoLampDevice_TurnOffOneLamp_1_SetsIsOnToFalseTheOtherRemainsFalse()
+        {
+            TwoLampDevice device = new TwoLampDevice();
+            device.TurnOnOneLamp(device.lamp1);
+            device.TurnOffOneLamp(device.lamp1);
+            Assert.False(device.lamp1.IsOn);
+            Assert.False(device.lamp2.IsOn);
+        }
+
+        [Fact]
+        public void TwoLampDevice_TurnOffOneLamp_2_SetsIsOnToFalseTheOtherRemainsFalse()
+        {
+            TwoLampDevice device = new TwoLampDevice();
+            device.TurnOnOneLamp(device.lamp2);
+            device.TurnOffOneLamp(device.lamp2);
+            Assert.False(device.lamp2.IsOn);
+            Assert.False(device.lamp1.IsOn);
+            
+        }
+
+        [Fact]
+        public void TwoLampDevice_TurnOnBothLamps_SetsIsOnToTrueToBothLamps()
         {
             TwoLampDevice device = new TwoLampDevice();
             device.TurnBothLampsOn();
             Assert.True(device.lamp1.IsOn);
             Assert.True(device.lamp2.IsOn);
         }
+
         [Fact]
-        public void TwoLampDevice_TurnOffBothLamps_SetsIsOnToFalse()
+        public void TwoLampDevice_TurnOffBothLamps_SetsIsOnToFalseToBothLamps()
         {
             TwoLampDevice device = new TwoLampDevice();
             device.TurnBothLampsOn();
@@ -43,6 +65,47 @@ namespace BlaisePascal.SmartHouse.Domain.UnitTest
             Assert.False(device.lamp1.IsOn);
             Assert.False(device.lamp2.IsOn);
         }
+
+        [Fact]
+        public void TwoLampDevice_TurnOnBothLamps_Lamp_1_AlreadyOn_SetsIsOnToTrueToBothLamps()
+        {
+            TwoLampDevice device = new TwoLampDevice();
+            device.TurnOnOneLamp(device.lamp1);
+            device.TurnBothLampsOn();
+            Assert.True(device.lamp1.IsOn);
+            Assert.True(device.lamp2.IsOn);
+        }
+
+        [Fact]
+        public void TwoLampDevice_TurnOnBothLamps_Lamp_2_AlreadyOn_SetsIsOnToTrueToBothLamps()
+        {
+            TwoLampDevice device = new TwoLampDevice();
+            device.TurnOnOneLamp(device.lamp2);
+            device.TurnBothLampsOn();
+            Assert.True(device.lamp1.IsOn);
+            Assert.True(device.lamp2.IsOn);
+        }
+
+        [Fact]
+        public void TwoLampDevice_TurnOffBothLamps_Lamp_1_AlreadyOff_SetsIsOnToFalseToBothLamps()
+        {
+            TwoLampDevice device = new TwoLampDevice();
+            device.TurnOnOneLamp(device.lamp2);
+            device.TurnBothLampsOff();
+            Assert.False(device.lamp1.IsOn);
+            Assert.False(device.lamp2.IsOn);
+        }
+
+        [Fact]
+        public void TwoLampDevice_TurnOffBothLamps_Lamp_3_AlreadyOff_SetsIsOnToFalseToBothLamps()
+        {
+            TwoLampDevice device = new TwoLampDevice();
+            device.TurnOnOneLamp(device.lamp1);
+            device.TurnBothLampsOff();
+            Assert.False(device.lamp1.IsOn);
+            Assert.False(device.lamp2.IsOn);
+        }
+
         [Fact]
         public void TwoLampDevice_SetBothSameBrightness_SetsBrightnessLevel()
         {
